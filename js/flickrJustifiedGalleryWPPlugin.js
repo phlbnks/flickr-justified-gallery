004bc4031fe3921d7c5b521e54d368c3c38f4391
Phil Banks <phil@helpfultechnology.com>
2015-07-15 10:11:14 +0100

    Initial commit (wp plugin v3.3.6)

---
+++ js/flickrJustifiedGalleryWPPlugin.js
@@ -0,0 +1,9 @@
+/* 
+Flickr Justified Gallery Wordpress Plugin
+Author: Miro Mannino
+Author URI: http://miromannino.com
+*/
+
+/* We save the instance of jQuery in case there are other themes or plugins that import again
+it in a non-standard way */
+var jQueryFJGWPP = jQuery;
